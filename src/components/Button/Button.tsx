89476d9f90c57c843a43aaa0ea188b612481d315
Artemii <rerttdtdf@gmail.com>
2026-03-07 14:06:18 +0300

    начинаем делать бутстрап

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,24 @@
+import type { JSXElement } from "@my-react/types/jsx"
+
+interface IButtonComponent {
+    text: string
+    icon: JSXElement
+    contentType: 'Icon' | 'Left+Text' | 'Text+Right' | 'Text'
+    kind: 'contained' | 'outlined' | 'ghost'
+    view: 'primary' | 'secondary' | 'negative'
+}
+
+export const Button = (props: IButtonComponent) => {
+
+    const {text, icon, contentType, kind,view} = props
+
+    const s = 2
+
+    return(
+        <button className={`
+        ${kind === 'ghost' && StyleSheet.buttonGhost}
+        `}>
+            {props.}
+        </button>
+    )
+}
