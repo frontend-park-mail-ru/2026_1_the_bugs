b6dbe9b6f95d61730248cc486641590127e5a17c
Artemii <rerttdtdf@gmail.com>
2026-03-14 14:49:10 +0300

    feat(oauth): start create Oauth page

---
+++ src/pages/OAuthVerifyPage.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from '@my-react/hooks';
+import { Header } from '../components/Header/Header';
+import { Hero } from '../components/Here/Here';
+
+import { authService } from '../services/auth';
+import type { IOAuthFlow } from 'src/types/api';
+
+
+/**
+ * Страница валидации OAuth
+ */
+export function OAuthVerifyPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+
+  const handleSearch = async () => {
+    const params = new URLSearchParams(window.location.pathname)
+    setIsLoading(true)
+    setIsError(false)
+    try{
+        await authService.loginFromVK(Object.fromEntries(params.entries()); as IOAuthFlow)
+    
+    }catch(err){
+        setIsError(true)
+    }finally{
+        setIsLoading(false)
+    }
+  };
+  useEffect(
+    ()=>{
+        handleSearch()
+    }, []
+  )
+
+  return (
+    <div className="page">
+    </div>
+  );
+}
