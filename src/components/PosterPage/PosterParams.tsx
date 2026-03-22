5e229e7dad186721ce754c7a704182de47a282b1
Andrey Fedotov <fedotov20.andrey04@gmail.com>
2026-03-22 19:32:50 +0300

    Poster page: step 5
    - make more minimalistic characteristic's block

--- src/components/PosterPage/PosterParams.tsx
+++ src/components/PosterPage/PosterParams.tsx
@@ -8,15 +8,14 @@ interface PosterParamsProps {
 
 export function PosterParams({ poster }: PosterParamsProps) {
   return (
-    <article className={shared.card}>
-      <h2 className={shared.title}>Характеристики</h2>
+    <article className={`${shared.card} ${styles.card}`}>
+      <h2 className={`${shared.title} ${styles.title}`}>Характеристики</h2>
       <div className={styles.params}>
         <div className={styles.param}><span className={styles.paramLabel}>Площадь</span><span className={styles.paramValue}>{poster.area.toString()} м²</span></div>
         <div className={styles.param}><span className={styles.paramLabel}>Этаж</span><span className={styles.paramValue}>{poster.flat.floor.toString()} из {poster.floor_count.toString()}</span></div>
         <div className={styles.param}><span className={styles.paramLabel}>Тип жилья</span><span className={styles.paramValue}>{poster.category}</span></div>
         <div className={styles.param}><span className={styles.paramLabel}>Категория</span><span className={styles.paramValue}>{poster.flat.flat_category}</span></div>
         <div className={styles.param}><span className={styles.paramLabel}>Номер квартиры</span><span className={styles.paramValue}>{poster.flat.flat_number.toString()}</span></div>
-        <div className={styles.param}><span className={styles.paramLabel}>ID объявления</span><span className={styles.paramValue}>{poster.id.toString()}</span></div>
       </div>
     </article>
   );
