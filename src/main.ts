8f59499755ec1ef73ddf28a580fe064abea96686
Artemiy <artemsikor18@gmail.com>
2026-04-11 01:28:35 +0300

    feat(web): add pwa and service worker

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { createApp } from '@my-react/my-react'
 import { App } from './App/App';
+import {registerSW} from "virtual:pwa-register";
 import "../index.css"
 
 const root = document.getElementById("app") as Element
 createApp(root, App);
+
+if ("serviceWorker" in navigator) {
+  registerSW()
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 // @ts-ignore (у меня ругался VS CODE)
 import eslint from 'vite-plugin-eslint';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   resolve: {
@@ -9,9 +10,34 @@ export default defineConfig({
     },
   },
   plugins: [
-    eslint({
-      failOnWarning: false,
-      failOnError: false,
-    }),
+    // eslint({
+    //   failOnWarning: false,
+    //   failOnError: false,
+    // }),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+       manifest:{
+        name: "ДомДели",
+        short_name: "DomDeli",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#f08dcc",
+        orientation: "portrait-primary",
+        icons: [
+          {
+            src: "/svg/logo.svg",
+            type: "image/svg+xml", "sizes": "192x192"
+          },
+          {
+            src: "/svg/logo.svg",
+            type: "image/svg+xml", "sizes": "512x512"
+          }
+        ]
+        }
+     })
   ],
 });
