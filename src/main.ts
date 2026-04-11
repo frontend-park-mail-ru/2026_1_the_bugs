f6e5cec9a1a0a4b4f1ad5a000993e446e3b06b45
Artemiy <artemsikor18@gmail.com>
2026-04-11 17:37:06 +0300

    fix(worker): change vite-worker into custom-worker

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { createApp } from 'the-react'
 import { App } from './App/App';
-import {registerSW} from "virtual:pwa-register";
 import "../index.css"
 
 const root = document.getElementById("app") as Element
 createApp(root, App);
 
 if ("serviceWorker" in navigator) {
-  registerSW()
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/serviceWorker.js");
+  })
 }
