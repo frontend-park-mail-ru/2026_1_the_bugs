63ab78a4da038295ef60558ec4b263f29caf6cac
Artemii <rerttdtdf@gmail.com>
2026-04-25 17:18:06 +0300

    add docer for support

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite';
 // @ts-ignore (у меня ругался VS CODE)
 import eslint from 'vite-plugin-eslint';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'support' ? '/support/' : '/',
+  build: {
+    rollupOptions: {
+      input: mode === 'support' ? 'support.html' : 'index.html'
+    }
+  },
   resolve: {
     alias: {
       '@router-dom': '/src/RouterDOM/index.ts', 
@@ -14,4 +20,4 @@ export default defineConfig({
       failOnError: false,
     }),
   ],
-});
+}));
